a4d041820030acb41db53c9cf4d8b089420ad636
Lawrence Norton <lawrence@lawrencenorton.com>
2015-02-05 15:58:34 -0500

    updated to call BCUS on fire

--- js/main.js
+++ js/main.js
@@ -1,41 +1,37 @@
 document.domain = "barc.com";
+    
+var signiniFrame = (function() {
+    'use strict';    
+    
+    var _cache = {},
+        BCUS = window.parent.BCUS,
+        
+        _init = function() {
+            _addCache();
+            _setupEvents();
+        },
+
+        _addCache = function() {
+            _cache.hiddenSignInEl = document.getElementsByClassName('login-panel__login-details')[0];
+            _cache.usernameInput = document.getElementsByClassName('login-panel__input')[0];
+            _cache.signInPanel = document.getElementsByClassName('login-panel')[0];
+            _cache.rememberUserCheckbox = document.getElementsByClassName('login-panel__label--checkbox')[0];
+        },
+
+        _setupEvents = function() {
+            _cache.usernameInput.addEventListener('click', _expandLogin);
+            
+        },
+
+        _expandLogin = function() {
+            _cache.hiddenSignInEl.style.display = 'block';
+            BCUS.HeaderMenu.loginExpanded();
+        };
+    return {
+        init: _init
+    };
+})();
 
 document.addEventListener('DOMContentLoaded', function() {
-    'use strict';    
-    if (window.parent && window.parent.BCUS) {
-        var signiniFrame = (function() {
-            var _cache = {},
-            _loginExpandCb = null,
-                
-                _init = function() {
-                    _addCache();
-                    _setupEvents();
-                },
-
-                _addCache = function() {
-                    _cache.hiddenSignInEl = document.getElementsByClassName('login-panel__login-details')[0];
-                    _cache.usernameInput = document.getElementsByClassName('login-panel__input')[0];
-                    _cache.signInPanel = document.getElementsByClassName('login-panel')[0];
-                    _cache.rememberUserCheckbox = document.getElementsByClassName('login-panel__label--checkbox')[0];
-                },
-
-                _setupEvents = function() {
-                    
-                },
-
-                _expandLogin = function() {
-                    this._loginExpandCb();
-                },
-
-                _onLoginExpand = function(callback) {
-                    _loginExpandCb = callback;
-                };
-
-            return {
-                init: _init,
-                onLoginExpand: _onLoginExpand
-            };
-        })();
-        signiniFrame.init();
-    }
+    signiniFrame.init();
 });
