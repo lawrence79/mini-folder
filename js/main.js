bcd993a4343f3096a7ba6ac693a99bad497f2eb3
Lawrence Norton <lawrence@lawrencenorton.com>
2015-02-05 15:03:43 -0500

    basic modules

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
 document.domain = "barc.com";
 
-document.addEventListener('DOMContentLoaded', function () {
-	console.log('Document Loaded');
-	if(window.parent && window.parent.BCUS) {
-		'use strict';
-		console.log('BCUS Module accessible');
+document.addEventListener('DOMContentLoaded', function() {
+    'use strict';    
+    if (window.parent && window.parent.BCUS) {
+        var signiniFrame = (function() {
+            var _cache = {},
+            _loginExpandCb = null,
+                
+                _init = function() {
+                    _addCache();
+                    _setupEvents();
+                },
 
-		var BCUS = window.parent.BCUS || {};
-		
-		BCUS.SignInModule = (function() {
-			var _cache = {};
+                _addCache = function() {
+                    _cache.hiddenSignInEl = document.getElementsByClassName('login-panel__login-details')[0];
+                    _cache.usernameInput = document.getElementsByClassName('login-panel__input')[0];
+                    _cache.signInPanel = document.getElementsByClassName('login-panel')[0];
+                    _cache.rememberUserCheckbox = document.getElementsByClassName('login-panel__label--checkbox')[0];
+                },
 
-		})
-	}
+                _setupEvents = function() {
+                    
+                },
+
+                _expandLogin = function() {
+                    this._loginExpandCb();
+                },
+
+                _onLoginExpand = function(callback) {
+                    _loginExpandCb = callback;
+                };
+
+            return {
+                init: _init,
+                onLoginExpand: _onLoginExpand
+            };
+        })();
+        signiniFrame.init();
+    }
 });
